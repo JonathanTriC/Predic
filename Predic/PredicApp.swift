8ec1e6bfe38b58d89898e0fedbba70f335a1b21f
Jonathan Tri <jonathantric@gmail.com>
2023-02-14 11:59:15 +0700

    Initial commit

---
+++ Predic/PredicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredicApp.swift
+//  Predic
+//
+//  Created by JonathanTriC on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PredicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Predic
+ Weather App
